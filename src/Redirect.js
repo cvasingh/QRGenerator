ad81a7c693290266657a7d065101ed6de86dabeb
Shiva Singh <shivasingh.200087@gmail.com>
2022-04-11 15:51:52 +0530

    fixed

--- src/Redirect.js
+++ src/Redirect.js
@@ -1,22 +1,19 @@
 import React from 'react'
-import { useParams,useNavigate } from 'react-router-dom';
-import { domainName, data } from './data'
+import { useParams, useNavigate } from 'react-router-dom';
 
-const Redirect = () => {
+const Redirect = ({ index, link }) => {
     const navigate = useNavigate();
-    var index = parseInt(useParams().index.replace(':', ''));
     console.log(index);
-    const link = data[index];
     console.log(link);
-    const handleLogin = ()=>{
+    const handleLogin = () => {
         navigate('/QRGenerator/login')
     }
     return <div class="contener">
-      <h1>Your QRs</h1>
-      <div class="signout">
-        <button class="btn btn-sm btn-light" type="submit" name="signout"
-          value="signout" onClick={handleLogin}>Login</button>
-      </div>
+        <h1>Your QRs</h1>
+        <div class="signout">
+            <button class="btn btn-sm btn-light" type="submit" name="signout"
+                value="signout" onClick={handleLogin}>Login</button>
+        </div>
         <div>{index}</div>
         <div>{link}</div>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Redirect from './Redirect';
 import NotFounds from './NotFounds';
+import { data } from './data'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='*' element={<NotFounds/>} />
-        <Route path='/QRGenerator/:index' element={<Redirect />} />
-        <Route path='/QRGenerator/login' element={<App />} />
+        <Route path='/QRGenerator' element={<App />} />
+
+        {data.map((item,index)=>{
+          return <Route path={`/QRGenerator/${index}`} element={<Redirect index={index} link={item}/>} />
+        })}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
